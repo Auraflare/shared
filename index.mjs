edbe058d9e88cbf1da4268bc30817118faca44a6
脆薯饼 <Virgil@nanocat.me>
2026-03-18 15:42:18 +0800

    chore: 收敛发包入口并限定 Cloudflare 与 KV 文件

--- index.mjs
+++ index.mjs
@@ -1,6 +1,3 @@
-export * from "./setENV.mjs";
-export * from "./database.mjs";
-export * from "./setCaches.mjs";
 export { default } from "./Cloudflare.mjs";
 export * from "./Cloudflare.mjs";
 export * from "./KV.mjs";
